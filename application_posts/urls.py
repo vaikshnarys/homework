525c6a74b4921216e880fdfa7e444cd0c27ddaac
vaikshnarys <dima.vaikshnarys@gmail.com>
2023-09-26 22:09:21 +0200

    ORM Django

--- application_posts/urls.py
+++ application_posts/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',new_post),
+   # path('',new_post),
+    path('',index),
+    path('about/',about, name = 'about'),
+    path('create_post/',create_new_post, name = 'create_post'),
+    path('get_post/<int:post_id>/',get_one_post, name = 'get_post'),
+    path('get_all_posts/', posts, name='all_posts'),
 ]
--- application_posts/views.py
+++ application_posts/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render,redirect
+menu = ['About','Add post','Return','Input']
+from application_posts.models import Post
+from django.views.decorators.csrf import csrf_exempt
 
+def index(request):
+    posts = Post.objects.all()
+    return render(request,'application_post/index.html',{'posts': posts,'menu':menu,'title' : 'Mean page news'})
+def about(request):
+    return render(request,'application_post/about.html',{'menu':menu,'title' : 'Mean page about website news'})
 NEW_POST = [
     {
         'title': 'First title',
@@ -22,3 +31,22 @@ NEW_POST = [
 
 def new_post(request):
     return JsonResponse(NEW_POST, safe = False)
+
+@csrf_exempt
+def create_new_post(request):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    post = Post(title = title,content = content)
+    post.save()
+    return HttpResponse(f"<h2>title: {title}  content {content}</h2>")
+
+def get_one_post(request,post_id):
+    post = Post.objects.get(id = post_id)
+    return HttpResponse(f"<h2>'id':{post.id} , title:{post.title}, content {post.content}</h2>")
+
+def posts(request):
+    list_posts = []
+    all_posts = Post.objects.all()
+    for post in all_posts:
+        list_posts.append([post.id, post.title, post.content])
+    return JsonResponse({'all_posts': list_posts})
